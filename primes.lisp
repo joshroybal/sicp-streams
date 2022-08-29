a6bc7e49d8e8fb05ea9d961210e20c97a2e8abbe
Joshua E. Roybal <u4ysev@gmail.com>
2022-08-28 20:35:52 -0500

    Add files via upload
    Changes to prime-factorization procedure lead to empirically verified massive performance improvement.

--- primes.lisp
+++ primes.lisp
@@ -8,18 +8,31 @@
   (:export :prime-p :twin-prime-p :prime-factorization))
 (in-package :primes)
 
+;;;
+(defun divides-p (x y)
+  "Procedure predicate is true when x|y."
+  (= (rem y x) 0))
+
+;;; 
+(defun smallest-divisor (n)
+  "Procedure returns smallest non-unity divisor of n."
+  (cond ((< n 2) nil)
+	((evenp n) 2)
+	((divides-p 3 n) 3)
+	(t
+	 (do ((k 5 (+ k 6)))
+	     ((> k (sqrt n)) n)
+	   (cond ((divides-p k n)
+		  (return-from smallest-divisor k))
+		 ((divides-p (+ k 2) n)
+		  (return-from smallest-divisor (+ k 2))))))))
+
 ;;; primality predicate
 (defun prime-p (n)
   "Procedure predicate is true when n is prime."
-  (cond ((< n 2) nil)
-	((< n 4) t)
-	((evenp n) nil)
-	((zerop (rem n 3)) nil)
-	(t
-	 (do ((k 5 (+ k 6)))
-	     ((> k (sqrt n)) t)
-	   (when (or (zerop (rem n k)) (zerop (rem n (+ k 2))))
-	     (return-from prime-p nil))))))
+  (if (= (smallest-divisor n) n)
+      t
+      nil))
 
 ;;; twin prime predicate
 (defun twin-prime-p (n)
@@ -33,20 +46,25 @@
   "Procedure constructs list representing prime-factors of n."
   (labels ((prime-factorization-recur (k primes-stream result)
 	     (cond ((equal (reduce #'* result) n)
-		    result)
-		   ((zerop (rem k (stream-car primes-stream)))
+		    (reverse result))
+		   ((prime-p k)
+		    (prime-factorization-recur
+		     (/ k (reduce #'* result))
+		     (stream-filter
+		      #'prime-p
+		      (integers-starting-from (+ k 2)))
+		     (cons k result)))
+		   ((divides-p (stream-car primes-stream) k)
 		    (prime-factorization-recur
 		     (/ k (stream-car primes-stream))
 		     primes-stream
-		     (append result (list (stream-car primes-stream)))))
+		     (cons (stream-car primes-stream) result)))
 		   (t
 		    (prime-factorization-recur
 		     k
 		     (stream-cdr primes-stream)
 		     result)))))
-    (if (prime-p n)
-	(list n)
-	(prime-factorization-recur
-	 n
-	 (stream-filter #'prime-p (integers-starting-from 2))
-	 nil))))
+    (prime-factorization-recur
+     n
+     (stream-filter #'prime-p (integers-starting-from (smallest-divisor n)))
+     nil)))
